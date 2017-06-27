4128318370f0dfca005a64107b72f9300da07b89
Titus Fortner <titusfortner@gmail.com>
2017-06-27 12:41:15 -0700

    fix syntax

--- lib/sauce_whisk.rb
+++ lib/sauce_whisk.rb
@@ -110,9 +110,9 @@ module SauceWhisk
   end
 
   def self.load_first_found(key)
-    self.instance_variable_get "@#{key}".to_sym ||
-                                   self.from_yml(key) ||
-                                   ENV["SAUCE_#{key.to_s.upcase}"]
+    self.instance_variable_get("@#{key}".to_sym) ||
+        self.from_yml(key) ||
+        ENV["SAUCE_#{key.to_s.upcase}"]
   end
 
   class JobNotComplete < StandardError
